4e19552149747a2f49dac325adfab2371bc65a77
nikitalu <quickper.nikitalu@gmail.com>
2022-07-17 18:18:46 +0800

    [update] add comma

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from 'path'
 export default defineConfig({
     resolve: {
         alias: {
-            '@' : '/src'
+            '@' : '/src',
         }
     },
     plugins: [vue()]
