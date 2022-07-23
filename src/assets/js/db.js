23b75b4b3d275fb0d3915d9e424ac39a00c26cc4
nikitalu <quickper.nikitalu@gmail.com>
2022-07-23 18:38:45 +0800

    [update]get set firebase realtime database

--- src/assets/js/db.js
+++ src/assets/js/db.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getDatabase, ref, set, child, get } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,14 +13,31 @@ const firebaseConfig = {
     storageBucket: "viteproject-b23b7.appspot.com",
     messagingSenderId: "1044190813086",
     appId: "1:1044190813086:web:86e820139894c3447e148f",
-    measurementId: "G-KWCWK6EL9Y"
+    measurementId: "G-KWCWK6EL9Y",
+    databaseURL: "https://viteproject-b23b7-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { auth, db, storage };
+export function setData(path, data) {
+    const db = getDatabase(app);
+    set(ref(db, path), data);
+}
+
+export function getData(path) {
+    const dbRef = ref(getDatabase(app));
+    return new Promise((resolve, reject) => {
+        get(child(dbRef, path)).then((snapshot) => {
+            if (snapshot.exists()) {
+                resolve(snapshot.val());
+            } else {
+                resolve(null);
+            }
+        }).catch((error) => {
+            resolve(null);
+        });
+    });
+
+}
+
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,65 @@
 <script>
-import { db } from '../assets/js/db.js'
+import {setData, getData} from '../assets/js/db.js'
+import {ref,reactive} from 'vue'
+
+function getRandom(x){
+    return Math.floor(Math.random()*x)+1;
+}
 
 export default {
     props: {},
     setup(props) {
-        return {}
+        let dataAry = reactive([]);
+        let dataUserId = ref(null);
+        let userName = ref(null);
+        let userEmail = ref(null);
+        let userId = getRandom(99);
+
+        const updateField = () => {
+            let apiPath = `users/${userId}`;
+            setData(apiPath, {
+                name: userName.value,
+                email: userEmail.value
+            });
+        }
+
+        const getDataFromDB = async () => {
+            let apiPath = `users/${dataUserId.value}`;
+            let resData = await getData(apiPath);
+
+            if (resData) {
+                dataAry.push(resData);
+            } else {
+                dataAry = [];
+            }
+        }
+
+        return {
+            dataAry,
+            dataUserId,
+            userName,
+            userEmail,
+            updateField,
+            getDataFromDB
+        }
     }
 }
 
 </script>
 
 <template>
-   <div class="homebox shadow-md">
-        hello
-   </div>
+    <label for="name">姓名:</label>
+    <input type="text" id="name" v-model="userName">
+    <label for="name">Email:</label>
+    <input type="text" id="email" v-model="userEmail">
+    <button type="button" @click="updateField">更新資料</button>
+    <br>
+    <label for="dataUserId">UserId:</label>
+    <input type="number" id="dataUserId" v-model="dataUserId">
+    <button type="button" @click="getDataFromDB">讀取資料</button>
+    <div class="homebox shadow-md">
+        {{ dataAry }}
+    </div>
 </template>
 
 <style scoped>
